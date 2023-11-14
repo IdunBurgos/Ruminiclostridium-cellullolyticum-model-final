8b3485583eef51ee11da33364e088516be20d6cf
Daniel Machado <cdanielmachado@gmail.com>
2023-11-14 09:28:36 +0100

    reorganize more

--- scripts/carve_draft.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-carve --refseq GCF_000022065.1 -o ../models/RcH10_draft.xml -u grampos --fbc2 --gapfill MM_xylose,MM_arabinose,MM_glucose,MM_galactose,MM_mannose,MM_cellobiose,DM_cellobiose,DM_cellulose,DM_xyloglucan,DM_cellulose_2,BM_xylan  -i MM_glucose --mediadb ../input/media_db.tsv -v -d --soft ../input/soft_constraints.tsv --hard ../input/hard_constraints.tsv 
